1a7307dd7cf93eeddf52321211ba7f64f0e50a81
Laur Sisask <laur.sisask@glia.com>
2019-07-12 13:12:11 +0300

    Bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TeslaMiddlewareTapper.MixProject do
   def project do
     [
       app: :tesla_middleware_tapper,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
